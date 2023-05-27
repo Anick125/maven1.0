82b6096781318d99db3893d9be5e55719a661132
anick <anick@anicks-mbp>
2023-05-27 00:54:41 -0400

    okk

---
+++ src/Practice1/Practiceabstraction.java
@@ -0,0 +1,38 @@
+package Practice1;
+
+public class Practiceabstraction  {
+	
+	
+	public void add (int a , int b) {
+		
+		int sum = (a+b);
+		System.out.println(sum);
+		
+	}
+	
+	public void multiply (int a, int b, int c) {
+		
+		int sum = (a*b*c);
+		System.out.println(sum);
+	}
+	
+public void name (String anick, String akash ) {
+	
+	String add = ("anick" + "akash");
+	System.out.println(add);
+	
+}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+
+		// TODO Auto-generated method stub
+		
+	}
+
+
