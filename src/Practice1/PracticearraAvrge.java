b7a93ab42b962df4615f14813bc4f2453bec545d
anick <anick@anicks-mbp>
2023-05-27 00:53:04 -0400

    ss

---
+++ src/Practice1/PracticearraAvrge.java
@@ -0,0 +1,39 @@
+package Practice1;
+
+public class PracticearraAvrge {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] num = {20,31,39,40};
+		
+		double sum = 0.0;
+		
+		for (int i = 0; i<num.length; i++ ) {
+			
+			sum = sum + num[i];
+			
+			
+	
+			
+			
+			
+			
+		}
+		
+		System.out.println(sum);
+		
+		double avrg = 0;
+		
+		avrg = sum/num.length;
+		
+		System.out.println(avrg);
+		
+		
+		
+		
+		
+	}
+
+}
