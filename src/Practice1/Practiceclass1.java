50d40932de09e33eab581d8c8d647cd1920a1245
anick <anick@anicks-mbp>
2023-05-27 00:51:21 -0400

    ok

---
+++ src/Practice1/Practiceclass1.java
@@ -0,0 +1,21 @@
+package Practice1;
+
+public class Practiceclass1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = 1000;
+		int num2 = 2000;
+
+		if (num2 < num1)
+		 	System.out.println("num1 is bigger");
+
+		else
+			System.out.println("num2 is bigger");
+
+		System.out.println("anick");
+
+	}
+
+}
