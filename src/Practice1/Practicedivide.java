e26d639ce78d6b073ade21a4c752089cb4f66076
anick <anick@anicks-mbp>
2023-05-27 00:31:40 -0400

    practice

---
+++ src/Practice1/Practicedivide.java
@@ -0,0 +1,17 @@
+package Practice1;
+
+public class Practicedivide {
+	
+	
+	public void divide (int a, int b) {
+		int sum = (a/b);
+		System.out.println(sum);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
