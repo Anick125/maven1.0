6f416419ca33c5e7b56830521f84214001e18e80
anick <anick@anicks-mbp>
2023-05-27 00:25:32 -0400

    commit

---
+++ src/Practice1/Ifelsepractice.java
@@ -0,0 +1,36 @@
+package Practice1;
+
+public class Ifelsepractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		/*int x = 10;
+		int y = 10;
+		
+		if (x>y)
+		
+		System.out.println("x is bigger");
+		
+		else if (y>x)
+			
+	System.out.println("y is bigger");
+		else if (x==y)
+			System.out.println("both are same");
+		else
+			System.out.println("error");*/
+		
+		/*for (int i = 0; i<10; i++) {
+			
+			System.out.println("anick");
+		}*/
+		
+		for (int i=0; i<5;i++) {
+			System.out.println("a");
+			
+		}
+		
+	}
+
+}
